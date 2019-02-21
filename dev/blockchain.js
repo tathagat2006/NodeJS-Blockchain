d25ae5fcf3af7e18757ef3d82e11af29f65f4563
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-21 15:17:55 +0530

    create new block method
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -3,3 +3,20 @@ function Blockchain() {
   this.chain = []; //mined blocks will be placed here as a chain
   this.newTransactions = []; //before mining the blocks will be placed here
 }
+
+//creating a new block
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+  const newBlock = {
+    index: this.chain.lenght + 1,
+    timeStamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash
+  };
+
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
