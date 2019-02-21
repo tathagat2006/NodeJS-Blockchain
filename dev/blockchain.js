fefd128dfe72b7486948852ebb3a454ed4235dfa
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-21 15:10:37 +0530

    blockchain constructor function
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ dev/blockchain.js
@@ -0,0 +1,5 @@
+//constructor function
+function Blockchain() {
+  this.chain = []; //mined blocks will be placed here as a chain
+  this.newTransactions = []; //before mining the blocks will be placed here
+}
