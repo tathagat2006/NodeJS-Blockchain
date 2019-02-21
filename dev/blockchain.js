cf0a3b1c2942c776c65ef5f2fcb6b2d4261171af
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-21 16:00:10 +0530

    creating a new/pending transaction
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,7 +1,7 @@
 //constructor function
 function Blockchain() {
   this.chain = []; //mined blocks will be placed here as a chain
-  this.newTransactions = []; //before mining the blocks will be placed here
+  this.pendingTransactions = []; //before mining the blocks will be placed here
 }
 
 //creating a new block
@@ -9,13 +9,13 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   const newBlock = {
     index: this.chain.length + 1,
     timeStamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash: previousBlockHash
   };
 
-  this.newTransactions = [];
+  this.pendingTransactions = [];
   this.chain.push(newBlock);
 
   return newBlock;
@@ -24,6 +24,18 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
 //get last block
 Blockchain.prototype.getLastBlock = function() {
   return this.chain[this.chain.length - 1];
+};
+
+//creating new/pending transactions to be mined
+Blockchain.prototype.crateNewTransaction = function(amount, sender, recipient) {
+  const newTransaction = {
+    amount,
+    sender,
+    recipient
+  };
+
+  this.pendingTransactions.push(newTransaction);
+  return this.getLastBlock()["index"] + 1; //number of the block that the above transaction will be added to
 };
 
 module.exports = Blockchain;
