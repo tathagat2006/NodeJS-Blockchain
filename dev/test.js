5750af4b5b7d12a483828d0eeb853d1a91158c26
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-21 16:28:11 +0530

    testing create new transaction and create new block
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,15 @@ const Blockchain = require("./blockchain");
 
 const bitcoin = new Blockchain();
 bitcoin.createNewBlock(2389, "0YUSJHA973ASHA", "90SAJSASV6246ASHJ");
+// bitcoin.createNewBlock(22329, "0YUDSDSDA7873ASHA", "73HJVDJAS893");
+// bitcoin.createNewBlock(3784, "9023JSBFJSDKJ", "9382BDJFSDBHFJ");
+bitcoin.crateNewTransaction(100, "ALEX89HJVDHAJVA786", "JEN65567ASGHAVGVDJV09");
+bitcoin.createNewBlock(22329, "0YUDSDSDA7873ASHA", "73HJVDJAS893"); //mining of the block created above
+
+bitcoin.crateNewTransaction(50, "JHAVSD7834KJD", "KJASDBAK73283723");
+bitcoin.crateNewTransaction(300, "23JHEVDJHADA", "KLNDHBGASVDFYAC3762");
+bitcoin.crateNewTransaction(2000, "GVASYD35723", "DNSBDV232736732");
+
 bitcoin.createNewBlock(22329, "0YUDSDSDA7873ASHA", "73HJVDJAS893");
-bitcoin.createNewBlock(3784, "9023JSBFJSDKJ", "9382BDJFSDBHFJ");
+
 console.log(bitcoin);
