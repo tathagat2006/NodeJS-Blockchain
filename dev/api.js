7f47b21c9d98c9487d09ee5f3ef699a1fd5030c1
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-23 01:22:45 +0530

    building api foundation
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/api.js
+++ dev/api.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+//get blockchain
+app.get("/blockchain", (req, res) => {
   res.send("Hello & Welcome");
 });
+
+//create new transaction
+app.post("/transaction", (req, res) => {});
+
+//mine a new block for us
+app.get("mine", (req, res) => {});
 
 app.listen("3000", () => {
   console.log("server started at http://localhost:3000");
