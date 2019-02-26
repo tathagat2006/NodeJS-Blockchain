770008809eced690bbc3073f4f29dd872e499a89
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-26 06:51:57 +0530

    building /consensus endpoint
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -185,6 +185,52 @@ app.post("/register-nodes-bulk", function(req, res) {
   res.json({ note: "Bulk registration successful." });
 });
 
+// consensus
+app.get("/consensus", function(req, res) {
+  const requestPromises = [];
+  bitcoin.networkNodes.forEach(networkNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/blockchain",
+      method: "GET",
+      json: true
+    };
+
+    requestPromises.push(rp(requestOptions));
+  });
+
+  Promise.all(requestPromises).then(blockchains => {
+    const currentChainLength = bitcoin.chain.length;
+    let maxChainLength = currentChainLength;
+    let newLongestChain = null;
+    let newPendingTransactions = null;
+
+    blockchains.forEach(blockchain => {
+      if (blockchain.chain.length > maxChainLength) {
+        maxChainLength = blockchain.chain.length;
+        newLongestChain = blockchain.chain;
+        newPendingTransactions = blockchain.pendingTransactions;
+      }
+    });
+
+    if (
+      !newLongestChain ||
+      (newLongestChain && !bitcoin.chainIsValid(newLongestChain))
+    ) {
+      res.json({
+        note: "Current chain has not been replaced.",
+        chain: bitcoin.chain
+      });
+    } else {
+      bitcoin.chain = newLongestChain;
+      bitcoin.pendingTransactions = newPendingTransactions;
+      res.json({
+        note: "This chain has been replaced.",
+        chain: bitcoin.chain
+      });
+    }
+  });
+});
+
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}...`);
 });
