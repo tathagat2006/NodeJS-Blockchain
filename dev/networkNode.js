8cd5d210581efaf2536d0f0ef9fb002c4b3a814b
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-26 06:33:06 +0530

    updating mine endpoint for broadcasting the mined node through network
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -68,12 +68,39 @@ app.get("/mine", (req, res) => {
     currentBlockData,
     nonce
   );
-  bitcoin.crateNewTransaction(12.5, "00", nodeAddress); // mining reward
+  // bitcoin.crateNewTransaction(12.5, "00", nodeAddress); // mining reward
   const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
-  res.json({
-    note: "New block mined successfully",
-    block: newBlock
+  const requestPromises = [];
+  bitcoin.networkNodes.forEach(newNodeUrl => {
+    const requestOptions = {
+      uri: networkNodeUrl + "/recieve-new-block",
+      method: "POST",
+      body: { newBlock: newBlock },
+      json: true
+    };
+    requestPromises.push(rp(requestOptions));
   });
+
+  Promise.all(requestPromises)
+    .then(data => {
+      const requestOptions = {
+        uri: bitcoin.currentNodeUrl + "/transaction/broadcast",
+        method: "POST",
+        body: {
+          amount: 12.5,
+          sender: "00",
+          recipient: nodeAddress
+        },
+        json: true
+      };
+      return rp(requestOptions);
+    })
+    .then(data => {
+      res.json({
+        note: "New block mined & broadcasted successfully",
+        block: newBlock
+      });
+    });
 });
 
 // register a node and broadcast it the network
