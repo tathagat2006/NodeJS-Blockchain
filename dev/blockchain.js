b70513755be30b7eb406b88218f7328768205324
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-02-26 06:47:35 +0530

    checking the chain is valid or not?
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -82,4 +82,42 @@ Blockchain.prototype.proofOfWork = function(
   return nonce; //we get the correct nonce value. our nonce is the proof of work.
 };
 
+Blockchain.prototype.chainIsValid = function(blockchain) {
+  let validChain = true;
+
+  for (var i = 1; i < blockchain.length; i++) {
+    const currentBlock = blockchain[i];
+    const prevBlock = blockchain[i - 1];
+    const blockHash = this.hashBlock(
+      prevBlock["hash"],
+      {
+        transactions: currentBlock["transactions"],
+        index: currentBlock["index"]
+      },
+      currentBlock["nonce"]
+    );
+    if (blockHash.substring(0, 4) !== "0000") {
+      validChain = false;
+    }
+    if (currentBlock["previousBlockHash"] !== prevBlock["hash"]) {
+      validChain = false;
+    }
+  }
+
+  const genesisBlock = blockchain[0];
+  const correctNonce = genesisBlock["nonce"] === 100;
+  const correctPreviousBlockHash = genesisBlock["previousBlockHash"] === "0";
+  const correctHash = genesisBlock["hash"] === "0";
+  const correctTransactions = genesisBlock["transactions"].length === 0;
+
+  if (
+    !correctNonce ||
+    !correctPreviousBlockHash ||
+    !correctHash ||
+    !correctTransactions
+  )
+    validChain = false;
+
+  return validChain;
+};
 module.exports = Blockchain;
